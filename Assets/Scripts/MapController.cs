f98a8289c32b30a37d7f34dd2ab0e4a26d813dc2
kevin-d-omara <kevin.d.omara@gmail.com>
2016-11-18 23:56:16 -0800

    added map + player basics

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour
+{
+    string url = "";
+    public float lat = 32.7157f;
+    public float lon = 117.1611f;
+
+    LocationInfo li;
+    public int zoom = 19; // zoom level; 0 = entire world; 21+ individual street/buildings
+
+    // max 640 by 640 pixels
+    public int mapWidth = 640;
+    public int mapHeigh = 640;
+
+    public enum mapType { roadmap, satellite, hybrid, terrain };
+    public mapType mapSelected;
+
+    public int scale; // scale can be 1,2 for free plan and can also be 4 for paid
+    IEnumerator GetGoogleMap()
+    {
+        url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon +
+                    "&zoom=" + zoom + "&size=" + mapWidth + "x" + mapHeigh + "&scale=" + scale
+                    + "&maptype=" + mapSelected +
+                    "&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284";
+        WWW www = new WWW(url);
+        yield return www;
+
+        Texture2D textureToReplace = gameObject.GetComponent<SpriteRenderer>().sprite.texture;
+        www.LoadImageIntoTexture(textureToReplace);
+    }
+
+    void Start()
+    {
+        StartCoroutine(GetGoogleMap());
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private float lat;
+    private float lon;
+
+    [SerializeField]
+    private MapController mapTile;
+
+    void Awake()
+    {
+        GetCoordinates(ref lat, ref lon);
+    }
+
+    private void Update()
+    {
+        GetCoordinates(ref lat, ref lon);
+        //mapTile.UpdateMap(lat, lon);
+    }
+
+    // fetch player's latitude and longitude in real time
+    private void GetCoordinates(ref float lat, ref float lon)
+    {
+        lat = 32.7157f;
+        lon = 117.1611f;
+    }
+}
